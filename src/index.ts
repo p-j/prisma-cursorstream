75d597ed815142e7adfb6d76799298eabfbaccd1
Jérémie Parker <hi@pkr.sh>
2024-12-03 17:38:22 +0100

    fix(strictUndefinedChecks): ensure cursor is defined

--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,69 @@
-import { Prisma, PrismaClientExtends } from "@prisma/client/extension";
-import { DefaultArgs } from "@prisma/client/runtime/library";
+import { Prisma, PrismaClientExtends } from '@prisma/client/extension'
+import { DefaultArgs } from '@prisma/client/runtime/library'
 
-import { Readable } from "node:stream";
+import { Readable } from 'node:stream'
 
-export default Prisma.defineExtension(
-  (client: PrismaClientExtends<DefaultArgs>) => {
-    return client.$extends({
-      model: {
-        $allModels: {
-          cursorStream<
-            T,
-            A extends Prisma.Args<T, "findMany"> | undefined,
-            R extends Prisma.Result<T, A, "findMany">[number],
-            C extends ((dataset: R[]) => Promise<unknown[]>) | undefined
-          >(
-            this: T,
-            findManyArgs: A,
-            { batchSize, prefill, batchTransformer } = {} as {
-              batchSize?: number;
-              prefill?: number;
-              batchTransformer?: C;
-            }
-          ): Iterable<
-            C extends Function
-              ? Awaited<ReturnType<C>>[number] extends object
-                ? Awaited<ReturnType<C>>[number]
-                : R
-              : R
-          > {
-            findManyArgs = findManyArgs ?? ({} as A);
-            const context = Prisma.getExtensionContext(this);
+export default Prisma.defineExtension((client: PrismaClientExtends<DefaultArgs>) => {
+  return client.$extends({
+    model: {
+      $allModels: {
+        cursorStream<
+          T,
+          A extends Prisma.Args<T, 'findMany'> | undefined,
+          R extends Prisma.Result<T, A, 'findMany'>[number],
+          C extends ((dataset: R[]) => Promise<unknown[]>) | undefined
+        >(
+          this: T,
+          findManyArgs: A,
+          { batchSize, prefill, batchTransformer } = {} as {
+            batchSize?: number
+            prefill?: number
+            batchTransformer?: C
+          }
+        ): Iterable<
+          C extends Function ? (Awaited<ReturnType<C>>[number] extends object ? Awaited<ReturnType<C>>[number] : R) : R
+        > {
+          findManyArgs = findManyArgs ?? ({} as A)
+          const context = Prisma.getExtensionContext(this)
 
-            const take = batchSize || 100;
-            const highWaterMark = prefill || take * 2;
-            const cursorField =
-              Object.keys(findManyArgs.cursor || {})[0] || "id";
+          const take = batchSize || 100
+          const highWaterMark = prefill || take * 2
+          const cursorField = Object.keys(findManyArgs.cursor || {})[0] || 'id'
 
-            if (findManyArgs.select && !findManyArgs.select[cursorField]) {
-              throw new Error(`Must select cursor field "${cursorField}"`);
-            }
+          if (findManyArgs.select && !findManyArgs.select[cursorField]) {
+            throw new Error(`Must select cursor field "${cursorField}"`)
+          }
 
-            let cursorValue: number;
-            const readableStream = new Readable({
-              objectMode: true,
-              highWaterMark,
-              async read() {
-                try {
-                  const results = await (context as any).findMany({
-                    ...findManyArgs,
-                    take,
-                    skip: cursorValue ? 1 : 0,
-                    cursor: cursorValue
-                      ? {
-                          [cursorField]: cursorValue,
-                        }
-                      : undefined,
-                  });
-                  const transformedResults = batchTransformer
-                    ? await batchTransformer(results)
-                    : results;
-                  for (const result of transformedResults) {
-                    this.push(result);
-                  }
-                  if (results.length < take) {
-                    this.push(null);
-                    return;
-                  }
-                  cursorValue = (<any>results[results.length - 1])[cursorField];
-                } catch (err: any) {
-                  this.destroy(err);
+          let cursorValue: number
+          const readableStream = new Readable({
+            objectMode: true,
+            highWaterMark,
+            async read() {
+              try {
+                const results = await (context as any).findMany({
+                  ...findManyArgs,
+                  take,
+                  skip: cursorValue ? 1 : 0,
+                  ...(cursorValue ? { cursor: { [cursorField]: cursorValue } } : {}),
+                })
+                const transformedResults = batchTransformer ? await batchTransformer(results) : results
+                for (const result of transformedResults) {
+                  this.push(result)
                 }
-              },
-            });
+                if (results.length < take) {
+                  this.push(null)
+                  return
+                }
+                cursorValue = (<any>results[results.length - 1])[cursorField]
+              } catch (err: any) {
+                this.destroy(err)
+              }
+            },
+          })
 
-            return readableStream as any;
-          },
+          return readableStream as any
         },
       },
-    });
-  }
-);
+    },
+  })
+})
